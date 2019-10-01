7bcaa03500f82c590e22bcfd7650ec6203573018
enthye <37763349+enthye@users.noreply.github.com>
2019-10-01 15:24:12 +1300

    added food and healing/damage feature

---
+++ Assets/Scripts/HP Scripts/HealingScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingScript : MonoBehaviour
+{
+    public int healAmount = 1;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Healing: " + other);
+        PlayerHPController playerHP = other.GetComponent<PlayerHPController>();
+
+        if (playerHP != null)
+        {
+            print("is trig");
+            if (playerHP.GetHealth < playerHP.maxHealth)
+            {
+                playerHP.ChangeHealth(healAmount);
+                Destroy(gameObject); // destroy item used to heal
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/HP Scripts/PlayerHPController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerHPController : MonoBehaviour
+{
+    public int maxHealth = 10;
+    int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = 5;
+    }
+
+    public int GetHealth
+    {
+        get
+        {
+            return currentHealth;
+        }
+    }
+
+    // damage or heal a certain amount of health
+    public void ChangeHealth(int amount)
+    {
+        // change health by amount, cannot go below 0 and above maxHealth
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log("Health is: " + currentHealth + "/" + maxHealth);
+    }
+}
