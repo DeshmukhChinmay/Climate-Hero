b07699e616812737b82f50f66262c13edf08f0e6
kwu849 <kwu849@aucklanduni.ac.nz>
2019-10-05 10:14:49 +1300

    added victory screen and controls to pause screen

---
+++ Assets/Scripts/Main Menu Scripts/VictoryScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryScene : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    public void Next(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void MainMenu(){
+        SceneManager.LoadScene("MainMenu");
+    }
+}
