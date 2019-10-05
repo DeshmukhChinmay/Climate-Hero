56ee96d97781aa42fdbb091ec8f59cd3ca844d2f
jzha653 <jzha653@aucklanduni.ac.nz>
2019-10-05 14:18:13 +1300

    heart system
    upload heart system

--- Assets/Scripts/HP Scripts/Hearts.cs
+++ Assets/Scripts/HP Scripts/Hearts.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Hearts : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public int health;
+    public int numberOfHearts;
+    public Image[] hearts;
+    public Sprite heart;
     void Update()
     {
-        
+        for (int i=0; i < numberOfHearts; i++) {
+            if (i < numberOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else {
+                hearts[i].enabled = false;
+            }
+        }
+  
     }
 }
