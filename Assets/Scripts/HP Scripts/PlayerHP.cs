bf2932771f5c7a5f0ed9ef05c68da04d6cbda7e2
jzha653 <jzha653@aucklanduni.ac.nz>
2019-10-06 15:06:18 +1300

    Heart System works now

--- Assets/Scripts/HP Scripts/PlayerHP.cs
+++ Assets/Scripts/HP Scripts/PlayerHP.cs
@@ -4,11 +4,15 @@ public class PlayerHP : MonoBehaviour
 {
     public int maxHealth = 10;
     int currentHealth;
+    public int health;
+    private Hearts hearts;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = 5;
+        health = currentHealth;
+       // hearts.changeNumOfHeart(5);
     }
 
     public int GetHealth
@@ -23,7 +27,9 @@ public class PlayerHP : MonoBehaviour
     public void ChangeHealth(int amount)
     {
         // change health by amount, cannot go below 0 and above maxHealth
+        //hearts.changeNumOfHeart(currentHealth+amount);
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        health = currentHealth;
         Debug.Log("Health is: " + currentHealth + "/" + maxHealth);
     }
 }
