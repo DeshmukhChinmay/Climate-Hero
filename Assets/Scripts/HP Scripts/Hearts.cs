af1fb25fab7831a48bab529801166b40c0e566f5
DeshmukhChinmay <chinmay2207@gmail.com>
2019-10-06 16:38:50 +1300

    Revert "Merge branch 'master' into EnemyAI"
    This reverts commit e459687eda4992f4eb072871f9ddaa2996ab74d5, reversing
    changes made to fbbe99bb7396ed25a3f7afba6518ca18285381b9.

--- Assets/Scripts/HP Scripts/Hearts.cs
+++ Assets/Scripts/HP Scripts/Hearts.cs
@@ -5,95 +5,21 @@ using UnityEngine.UI;
 
 public class Hearts : MonoBehaviour
 {
-    [SerializeField] private Sprite heartSprite;
-    private List<HeartImage> hiList;
-    private Hearts h;
-    //private PlayerHP ph;
-    //public int health;
-    public int numberOfHearts=10;
-    private void Awake()
+    public int health;
+    public int numberOfHearts;
+    public Image[] hearts;
+    public Sprite heart;
+    void Update()
     {
-        hiList = new List<HeartImage>();
-    }
-
-
-    private void Start()
-    {
-        GameObject thePlayer = GameObject.Find("PlayerCollider");
-        PlayerHP playerScript = thePlayer.GetComponent<PlayerHP>();
-        numberOfHearts = playerScript.health;
-        Debug.Log("Heart is:========================== " + playerScript.health);
-        // int health = ph.GetHealth;
-        HeartImage image1 = createHeartImage(new Vector2(-385, 180));
-        HeartImage image2 = createHeartImage(new Vector2(-375, 180));
-        HeartImage image3 = createHeartImage(new Vector2(-365, 180));
-        HeartImage image4 = createHeartImage(new Vector2(-355, 180));
-        HeartImage image5 = createHeartImage(new Vector2(-345, 180));
-        HeartImage image6 = createHeartImage(new Vector2(-385, 170));
-        HeartImage image7 = createHeartImage(new Vector2(-375, 170));
-        HeartImage image8 = createHeartImage(new Vector2(-365, 170));
-        HeartImage image9 = createHeartImage(new Vector2(-355, 170));
-        HeartImage image10 = createHeartImage(new Vector2(-345, 170));
-    }
-    public void Update()
-    {      GameObject thePlayer = GameObject.Find("PlayerCollider");
-        PlayerHP playerScript = thePlayer.GetComponent<PlayerHP>();
-        numberOfHearts = playerScript.health;
-        //GameObject thePlayer = GameObject.Find("ThePlayer");
-        //PlayerHP playerScript = thePlayer.GetComponent<PlayerHP>();
-        //numberOfHearts = playerScript.health;
- 
-        //numberOfHearts = ph.getHealth();
-        for (int i = 0; i < 10; i++)
-        {
+        for (int i=0; i < numberOfHearts; i++) {
             if (i < numberOfHearts)
             {
-                hiList[i].Set(1);
+                hearts[i].enabled = true;
             }
-            else
-            {
-               // Debug.Log(hiList.Capacity);
-                hiList[i].Set(0);
+            else {
+                hearts[i].enabled = false;
             }
         }
-
-    }
-
-    private HeartImage createHeartImage(Vector2 anchorPosit)
-    {
-        GameObject go = new GameObject("Heart", typeof(Image));
-        go.transform.parent = transform;
-        go.transform.localPosition = Vector3.zero;
-
-        go.transform.GetComponent<RectTransform>().anchoredPosition = anchorPosit;
-        go.transform.GetComponent<RectTransform>().sizeDelta = new Vector2(10, 10);
-
-        Image heartImageUI = go.GetComponent<Image>();
-        heartImageUI.sprite = heartSprite;
-        HeartImage heartImage = new HeartImage(h, heartImageUI);
-        hiList.Add(heartImage);
-        return heartImage;
-    }
-
-    public class HeartImage
-    {
-        private Image heartImage;
-        private Hearts hearts;
-        public HeartImage(Hearts h, Image image)
-        {
-            heartImage = image;
-            hearts = h;
-        }
-        public void Set(int h)
-        {
-            if (h == 1)
-            {
-                heartImage.enabled = true;
-            }
-            else
-            {
-                heartImage.enabled = false;
-            }
-        }
+  
     }
 }
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # 306Project2
 Project 2 for SOFTENG 306. Team 9. This project is about building a serious game revolving around climate change. The target audience of the game is 12 year olds.
 
-## Team Members (name, GitHub username, UPI)
-- Brian Zhang - brianzhang310 - bzha841
-- Chinmay Deshmukh - DeshmukhChinmay - cdes685
-- Jiaqiu Zhao - jzha653 - jzha653
-- Kevin Xu - enthye - kxu015
-- Kungeng Wu - Kwu849 - kwu849
-- Lisa Wang - zwan940 - zwan940
-- Terence Qu - Terenco12345 - tqu593
+## Team Members
+- Brian Zhang
+- Chinmay Deshmukh
+- Jiaqiu Zhao
+- Kevin Xu
+- Kungeng Wu
+- Lisa Wang
+- Terence Qu
 
 
