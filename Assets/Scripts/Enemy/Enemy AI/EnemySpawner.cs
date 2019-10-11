a7c71c3eefe7305ea4fad90ad2e4cb1d33b745b4
enthye <37763349+enthye@users.noreply.github.com>
2019-10-11 23:13:47 +1300

    Fixed up project structure for final implementation.  - moved files to appropriate folders  - removed unnecessary files

---
+++ Assets/Scripts/Enemy/Enemy AI/EnemySpawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    public GameObject enemyObject;
+
+    public Transform[] spawnPoints; 
+
+    // private Vector3 spawnPosition;
+    // private Quaternion rotation = new Quaternion(0,0,0,0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        Debug.Log("This is getting run!!!!!!!!!!");
+
+        var values = new List<int>();
+
+        for (int i = 1; i <= spawnPoints.Length; i++) {
+            values.Add(i);
+            Debug.Log(spawnPoints.Length);
+        }
+
+        for (int j = 0; j < (spawnPoints.Length/2); j++) {
+            bool validNumber = false;
+            while(!validNumber) {
+                var number = Random.Range(0, spawnPoints.Length-1);
+                if (values.Contains(number)) {
+                    Instantiate(enemyObject, spawnPoints[number].position, spawnPoints[number].rotation);
+                    values.Remove(number); 
+                    validNumber = true;
+                }
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
