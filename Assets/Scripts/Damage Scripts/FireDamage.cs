132a4ed760c032e59529f9aa31115496bd8d174a
enthye <37763349+enthye@users.noreply.github.com>
2019-10-12 21:50:34 +1300

    fire area damage prefab implemented

---
+++ Assets/Scripts/Damage Scripts/FireDamage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireDamage : MonoBehaviour
+{
+    // when player walks on top of fire, they get damaged
+    void OnTriggerStay2D(Collider2D other)
+    {
+        PlayerHP playerHP = other.GetComponent<PlayerHP>();
+        if (playerHP != null)
+        {
+            if (playerHP.health > 0)
+            {
+                playerHP.ChangeHealth(-1);
+            }
+        }
+    }
+}
