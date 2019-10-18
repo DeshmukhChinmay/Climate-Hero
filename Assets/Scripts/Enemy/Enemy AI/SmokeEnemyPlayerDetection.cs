4a181061b996f5fb0e426e4f46670f9c39a9c150
DeshmukhChinmay <chinmay2207@gmail.com>
2019-10-19 12:38:04 +1300

    Created a prefab for the smoke from the smoke enemy

---
+++ Assets/Scripts/Enemy/Enemy AI/SmokeEnemyPlayerDetection.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeEnemyPlayerDetection : MonoBehaviour
+{
+
+    public GameObject smokeAttack;
+    public Transform firePoint;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 2);
+        Collider2D player = null;
+
+        for (int i = 0; i < colliders.Length; i++) {
+            if (colliders[i].tag == "Player") {
+                player = colliders[i];
+                Debug.Log("Smoke Enemy found the player!!!");
+                break;
+            }
+        }
+
+        if (player != null) {
+            Fire();
+        } else {
+            Debug.Log("Player not found!");
+        }
+    }
+
+    void Fire() {
+        Instantiate(smokeAttack, firePoint.position, firePoint.rotation);
+    }
+}
---
+++ Assets/Scripts/Enemy/Smoke.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smoke : MonoBehaviour
+{
+
+    private float speed = 10f;
+    // public RigidBody2D rigidBody; 
+
+    private Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        this.player = GameObject.FindWithTag("Player").transform;
+
+        Vector3 playerPos = new Vector3(player.position.x, player.position.y +1, player.position.z);
+
+        transform.rotation = Quaternion.LookRotation(playerPos);
+        transform.LookAt(player.position);   
+    }
+
+    void Update() {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+}
