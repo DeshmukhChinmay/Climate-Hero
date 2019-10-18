43f7b0259e264395e28d99281195bc4bdeb7231e
jzha653 <jzha653@aucklanduni.ac.nz>
2019-10-18 14:57:17 +1300

    fixed some bugs

---
+++ Assets/Scripts/Game Meta Scripts/FinishLevel 2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLevel : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("UPDATEEE");
+        if(GetComponent<EnemyHP>().GetHealth <= 1){
+            Debug.Log("DEAD>>>");
+            SceneManager.LoadScene("VictoryScene");
+        }
+    }
+}
---
+++ Assets/Scripts/Game Meta Scripts/PauseMenu 2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool IsPaused = false;
+    public GameObject PauseMenuUI;
+    
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if(IsPaused){
+                Resume();
+            } else{
+                Pause();
+            }
+        }
+    }
+    public void QuitGame(){
+        Debug.Log("Closing");
+        Application.Quit();
+    }
+
+    public void MainMenu(){
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Resume  (){
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+    void Pause(){
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+}
