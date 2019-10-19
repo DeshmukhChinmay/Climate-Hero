3f674baefaa0bb87e45f61698b3f0095f7f8b40f
jzha653 <jzha653@aucklanduni.ac.nz>
2019-10-19 14:03:58 +1300

    fixed the timer bugs,added 2 digits timer

--- Assets/Scripts/Player/Timer.cs
+++ Assets/Scripts/Player/Timer.cs
@@ -5,15 +5,28 @@ public class Timer : MonoBehaviour
 {
     public Text timerText;
     public float startTime;
-   void Start()
+    void Start()
     {
         startTime = Time.time;
     }
     void Update()
     {
         float t = Time.time - startTime;
+        string seconds;
         string minutes = ((int)t / 60).ToString();
-        string seconds = (t % 60).ToString("f0");
+        string time = (t % 60).ToString("f0");
+        if (int.Parse(time) == 60)
+        {
+            seconds = "00";
+            minutes = (((int)t / 60) + 1).ToString();
+        }
+        else if ((t % 60).ToString("f0").Length >= 2) { seconds = (t % 60).ToString("f0"); }
+        else
+        {
+            seconds = "0" + (t % 60).ToString("f0");
+        }
+
+
         timerText.text = minutes + ":" + seconds;
     }
 
