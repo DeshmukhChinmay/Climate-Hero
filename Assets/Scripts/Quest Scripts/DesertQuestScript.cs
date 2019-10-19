9b52a8769c590d21be84da4335347b9963e710d0
kwu849 <kwu849@aucklanduni.ac.nz>
2019-10-19 17:40:44 +1300

    created desert quest scripts

---
+++ Assets/Scripts/Quest Scripts/DesertQuestScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class DesertQuestScript : MonoBehaviour
+{
+    public static int totalObjectives = 8;
+    public static int completedObjectives = 0;
+    // Start is called before the first frame update
+    public static void completeObjective(){
+        completedObjectives += 1;
+        Debug.Log(completedObjectives);
+    }
+
+    public static bool checkStatus(){
+        if(totalObjectives == completedObjectives){
+            return true;
+        }
+        return false;
+    }
+}
